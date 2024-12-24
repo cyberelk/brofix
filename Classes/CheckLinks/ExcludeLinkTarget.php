ed31b6443a30639109bdd16c60ab0cb14e529455
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2024-12-24 11:48:21 +0100

    Update ExcludeLinkTarget.php

--- Classes/CheckLinks/ExcludeLinkTarget.php
+++ Classes/CheckLinks/ExcludeLinkTarget.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * The TYPO3 project - inspiring people to share!
  */
 
-namespace Sypets\Brofix\CheckLinks;
+namespace Cyberelk\Brofix\CheckLinks;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
