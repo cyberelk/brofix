5099e1455f86a2f2f7360e3dfa4a3ed377b3b098
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2024-12-24 12:34:11 +0100

    Update ContentRepository.php

--- Classes/Repository/ContentRepository.php
+++ Classes/Repository/ContentRepository.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Sypets\Brofix\Repository;
+namespace Cyberelk\Brofix\Repository;
 
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
